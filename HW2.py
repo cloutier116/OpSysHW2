45245bad21d8f65c9f04b300e968661dac546c1b
Chris <cloutc@rpi.edu>
2014-10-15 20:56:30 -0400

    made it work

--- HW2.py
+++ HW2.py
@@ -1,16 +1,18 @@
 import random
 
 class Process:
-	def __init__ (self, _pPum, _interative):
-		state = "ready"
-		pNum = pNum
-		interactive = _interactive
+	def __init__ (self, _pNum, _interactive):
+		self.state = 0 #0 = ready, 1 = active, 2 = blocked on I/O
+		self.pNum = _pNum
+		self.interactive = _interactive
 
 n = 12
 processes = []
 
 for i in range(1,n+1):
-	if(random.range(0,100) < 80):
-		processes[i] = Process(i, True)
+	if(random.randint(0,100) < 80):
+		processes.append(Process(i, True))
 	else:
-		processes[i] = Process(i, False)
+		processes.append(Process(i, False))
+
+print processes
