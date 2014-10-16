41ea07bff5b58158282675fe3567e45ed7cb7eb3
Parshva Shah <g7parsh@gmail.com>
2014-10-15 21:49:32 -0400

    moved thins to a mina fucntion. add a priority sysytem

--- HW2.py
+++ HW2.py
@@ -1,18 +1,24 @@
 import random
-
+import sys
 class Process:
-	def __init__ (self, _pNum, _interactive):
+	def __init__ (self, _pNum, _interactive,_priority = 0,):
 		self.state = 0 #0 = ready, 1 = active, 2 = blocked on I/O
 		self.pNum = _pNum
 		self.interactive = _interactive
+		self.priority = _priority
 
-n = 12
-processes = []
+	
 
-for i in range(1,n+1):
-	if(random.randint(0,100) < 80):
-		processes.append(Process(i, True))
-	else:
-		processes.append(Process(i, False))
 
-print processes
+
+
+if __name__ == '__main__':
+	n = 12
+	processes = []
+	for i in range(1,n+1):
+		if(random.randint(0,100) < 80):
+			processes.append(Process(i, True))
+		else:
+			processes.append(Process(i, False))
+
+	print processes
